fe852595e6ff64568466a6ccec2135603ef59c30
James Moriarty <jamespaulmoriarty@gmail.com>
2013-06-03 22:24:26 +1000

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,10 @@
+maintainer       "James Moriarty"
+maintainer_email "james@locomote.com"
+license          "MIT"
+description      "Installs / configure MiniDLNA"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
 %w(debian ubuntu).each do |os|
   supports os
 end
